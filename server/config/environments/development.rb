159e1779765944261dab2d3470e258d321d2a96f
jheiss <jheiss>
2008-10-13 22:28:49 +0000

    Turn caching back off, since I'm not working on developing the page that triggers the bug that caused me to turn it on. And having it on in development is really annoying.

--- server/config/environments/development.rb
+++ server/config/environments/development.rb
@@ -8,17 +8,17 @@
 # since you don't have to restart the webserver when you make code changes.
 # Besides the acts_as_audited note above, this needs to be set to true
 # for now to work around http://dev.rubyonrails.org/ticket/10896, which
-# gets triggered by the link_to call in _notes.html.erb file.
-#config.cache_classes = false
-config.cache_classes = true
+# gets triggered by the link_to call in _comments.html.erb file.
+config.cache_classes = false
+#config.cache_classes = true
 
 # Log error messages when you accidentally call methods on nil.
 config.whiny_nils = true
 
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false
 # See note about acts_as_audited above
+config.action_controller.perform_caching             = false
 #config.action_controller.perform_caching             = true
 config.action_view.debug_rjs                         = true
 
