425e0734a06e368f43c667bbe0799882a3220d93
jheiss <jheiss>
2008-10-13 23:40:49 +0000

    Switch node->rack and rack->datacenter assocations to has_one, :through assocations now that we're using a version of Rails (2.1) that supports that syntax.

--- server/app/models/rack.rb
+++ server/app/models/rack.rb
@@ -4,14 +4,10 @@ class Rack < ActiveRecord::Base
   acts_as_commentable
   
   has_one :datacenter_rack_assignment, :dependent => :destroy
-  # has_one :through support was recently added to Rails
-  # http://dev.rubyonrails.org/ticket/4756
-  # Once that makes it into a version we can run this can get
-  # uncommented and the datacenter method below can go away.
-  #has_one :datacenter, :through => :datacenter_rack_assignment
+  has_one :datacenter, :through => :datacenter_rack_assignment
   
   has_many :rack_node_assignments, :order => "position"
-  has_many :nodes, :through => :rack_node_assignments, :conditions => 'rack_node_assignments.deleted_at IS NULL'
+  has_many :nodes, :through => :rack_node_assignments
   
   validates_presence_of :name
   
@@ -33,15 +29,6 @@ class Rack < ActiveRecord::Base
   
   def free_u_height
     self.u_height - self.used_u_height
-  end
-  
-  # This can go away eventually, see above
-  def datacenter
-    if self.datacenter_rack_assignment
-     return self.datacenter_rack_assignment.datacenter
-    else
-      return nil
-    end
   end
   
   def before_destroy
