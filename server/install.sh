6285858be539327efb90e5d96eba7623f0cbfa7e
jheiss <jheiss>
2009-09-30 21:16:08 +0000

    Remove the -y flag from all lines, gem says it is deprecated.
    Fix a few duplicate lines and typos.

--- server/install.sh
+++ server/install.sh
@@ -3,21 +3,20 @@
 MYSQL_CONFIG_DIR="/usr/bin/mysql_config"
 RAILS_VER="2.1.2"
 
-gem install rails -v $RAILS_VER -y
-gem install RedCloth -v 3.0.4 -y
-gem install ruby-net-ldap -v 0.0.4 -y
-gem install ruport -y
-gem install acts_as_reportable -y
-gem install starling -y
-gem install fast_cs -y
-gem install fastercsv -y
-gem install facter -y
+gem install rails -v $RAILS_VER
+gem install RedCloth -v 3.0.4
+gem install ruby-net-ldap -v 0.0.4
+gem install ruport
+gem install acts_as_reportable
+gem install starling
+gem install fast_xs
+gem install fastercsv
+gem install facter
 ## If hpricot gem install fails, try lower version
-## Example:  gem install hpricot -v .7
-gem install hpricot -y
-gem install mongrel -y
-gem install mysql -y
-gem install mislav-will_paginate --source http://gems.github.com/ -v 2.3.2 -y
-gem install mongrel -v 1.1.5 -y
-gem install mysql -- --with-mysql-config=$MYSQL_CONFIG_DIR -y
+## Example:  gem install hpricot -v 0.7
+gem install hpricot
+gem install mongrel
+gem install mislav-will_paginate --source http://gems.github.com/ -v 2.3.2
+gem install mongrel -v 1.1.5
+gem install mysql -- --with-mysql-config=$MYSQL_CONFIG_DIR
 gem install ruby-graphviz
