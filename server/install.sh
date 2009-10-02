efcf2e11e800de25d612d3c6466edc5363c016a1
jhtran <jhtran>
2009-10-02 18:58:58 +0000

    adding some pre-requisite info

--- server/install.sh
+++ server/install.sh
@@ -6,14 +6,31 @@ MYSQL_CONFIG_DIR="/usr/bin/mysql_config"
 RAILS_VER="2.3.2"
 
 ## OPERATING SYSTEM PRE-REQUISITES
-# yum install mysql-server -y
-# yum install mysql-devel -y
-# yum install graphviz -y
 # yum install ruby -y
+# yum install mysql-server -y
+## for rubygems
 # yum install -y ruby-devel ruby-docs ruby-ri ruby-irb ruby-rdoc
+## for gems (hpricot & fastthread)
 # yum install -y gcc make
+## for graphviz gem
+# yum install graphviz -y
+## for mysql gem
+# yum install mysql-devel -y
+## for nginx
+# yum install -y gcc-c++
+# yum install -y zlib-devel
 
-# goto rubygems.org to download and install rubygems
+## goto rubygems.org to download and install rubygems (example below but you want the latest source)
+# wget http://rubyforge.org/frs/download.php/60718/rubygems-1.3.5.tgz
+# tar zxvf rubygems-1.3.5.tgz
+# cd rubygems-1.3.5
+# ruby setup.rb
+
+## get phusion-passenger nginx installation package (example below but you want the latest source)
+# wget http://rubyforge.org/frs/download.php/59007/passenger-2.2.4.tar.gz
+# tar zxvf passenger-2.2.4.tar.gz
+# cd passenger-2.2.4/bin
+# ./passenger-install-nginx-module
 
 gem install rails -v $RAILS_VER
 gem install RedCloth -v 3.0.4
