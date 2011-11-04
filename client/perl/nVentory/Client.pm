b503024d91db6740659cc67f7c6d8e70780691de
thepob <thepob>
2011-11-04 19:39:46 +0000

    during register we should do a match on the hardware serial number - this closes out bug #3

--- client/perl/nVentory/Client.pm
+++ client/perl/nVentory/Client.pm
@@ -1058,6 +1058,17 @@ sub register
                 }
 	}
 
+	# If we can't get a match on the uniqueid, we'll want to do a match
+	# based on the hardware serial number, if available. 
+	if (!%results && $data{serial_number})
+	{
+		my %getdata;
+		$getdata{'objecttype'} = 'nodes';
+		$getdata{'exactget'} = {'serial_number' => [$data{serial_number}]};
+		$getdata{'login'} = 'autoreg';
+		%results = get_objects(\%getdata);
+	}
+
 	# If we failed to find an existing entry based on the unique id
 	# fall back to the hostname.  This may still fail to find an entry,
 	# if this is a new host, but that's OK as it will leave %results
